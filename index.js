cc84296cf1a6cd62a0fb9fbab1b283b5517d2287
KeshavAptclouds <151923820+KeshavAptclouds@users.noreply.github.com>
2023-11-24 18:40:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 let searchBox = document.getElementById("search_box");
 
 
-searchBox.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
+searchBox.addEventListener("keyup", (e) => {
       e.preventDefault();
       handleSearch(e.target.value);
-    }
   });
   
   getResponse();
 
-   async function getResponse(){
+  let cities = [];
+
+  async function getResponse(){
     const url = 'https://fl6ikdsevf.execute-api.us-east-1.amazonaws.com/Testing';
-    const response = await fetch(url);
-    console.log(response);
+    fetch(url)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(data.body);
+      data.body.forEach((item) => {
+        cities.push(item.City);
+    });
+    })
    }
 
 
   function handleSearch(value){
-
     console.log(value);
+    let list = document.getElementById("myList");
+    list.innerHTML = "";
+    if(value != undefined && value != ''){
+      
+      let searchResult = cities.filter(item => (item.toLowerCase().startsWith(value.toLowerCase())));
+      searchResult.forEach( item => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        list.appendChild(li);
+      });
+    }
+    
   }
